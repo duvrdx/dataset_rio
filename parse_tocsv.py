d5f25077100eaf85257ee8edfcf8683b88e855a4
Eduardo Henrique <duvrdx@gmail.com>
2023-09-10 12:03:27 -0300

    first commit

---
+++ parse_tocsv.py
@@ -0,0 +1,46 @@
+import xml.etree.ElementTree as ET
+import pandas as pd
+
+
+def parse(file):
+    # Carregue o arquivo XML
+    tree = ET.parse(file)
+    root = tree.getroot()
+
+    # Crie listas para armazenar os dados
+    cod_estacao = []
+    data = []
+    hora = []
+    vazao = []
+    nivel = []
+    chuva = []
+
+    # Itere sobre os elementos do XML e extraia os dados
+    for dados in root.findall('.//DadosHidrometereologicos'):
+        cod_estacao.append(dados.find('CodEstacao').text)
+        data_hora = dados.find('DataHora').text.strip()
+        data.append(data_hora.split()[0])
+        hora.append(data_hora.split()[1])
+        vazao.append(dados.find('VazaoFinal').text)
+        nivel.append(dados.find('NivelFinal').text)
+        chuva.append(dados.find('ChuvaFinal').text)
+
+    # Crie um DataFrame pandas com os dados extraídos
+    df = pd.DataFrame({
+        'Data': data,
+        'Hora': hora,
+        'Vazao': vazao,
+        'Nivel': nivel,
+        'Chuva': chuva
+    })
+
+    df.to_csv(f"{file[:-4]}.csv")
+
+# Imprima o DataFrame resultante
+if __name__ == "__main__":
+    files = ["response.xml"]
+
+
+    for file in files:
+        parse(file)
+        print(f"'{file}' is done!")
